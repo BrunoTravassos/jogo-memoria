751ad9ffc19c05ebffdf6cf2c072cc15de11df60
BrunoTravassos <brunodefrontin@gmail.com>
2021-10-01 13:25:33 -0300

    commit jogo-da-memoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RestartComponent } from './restart';
+import { CartaAnimacaoComponent } from './carta-animacao/carta-animacao.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestartComponent,
+    CartaAnimacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
